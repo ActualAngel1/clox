de2ad9ebd34cd2eab4b0cdb79c8b3beca1e7719d
ActualAngel1 <115042939+ActualAngel1@users.noreply.github.com>
2023-03-25 16:52:26 +0300

    added hash tables and sped == on strings up

--- getLine.c
+++ getLine.c
@@ -1,13 +1,1 @@
-int getLine(int offset, int* lines){
-  offset++;
-  int sum = 0;
-  int* copy = lines;
-  copy++;
-  int i = 1;
-  while(sum < offset){
-      sum+=*copy;
-      copy+=2;
-      i++;
-  }
-  return i;
-}
+
--- getLine.h
+++ getLine.h
@@ -1,10 +1,1 @@
-#ifndef clox_getLine_h
-#define clox_getLine_h
 
-#define currentLine chunk->lines[(getLine(offset, chunk->lines)-2)*2]
-#define nextLine chunk->lines[(getLine(offset + 1, chunk->lines)-2)*2]
-#define prevLine chunk->lines[(getLine(offset - 1, chunk->lines)-2)*2]
-
-int getLine(int offset, int* lines);
-
-#endif
